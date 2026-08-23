e5c145669f29b28ccd55f0685f519d15dd786c33
MDen <danilalikan@gmail.com>
2026-08-24 04:23:15 +0500

    feat: improve lightning

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+
 import {getPositions} from './celestial-bodies-positions.js';
 import {interpolatePosition} from './utils.js';
 
@@ -10,6 +11,8 @@ const Interval10Minutes = 1 / 600000;
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
@@ -25,8 +28,15 @@ controls.minDistance = 2;
 const loader = new GLTFLoader();
 const textureLoader = new THREE.TextureLoader();
 
-const AmbientLight = new THREE.AmbientLight(0xffffff, 0.005);
+const AmbientLight = new THREE.AmbientLight(0xffc2b3, 0.003);
 scene.add(AmbientLight);
+
+const spotLight = new THREE.SpotLight(0xffc2b3, 0.3);
+spotLight.position.set(0, 0, 0);
+spotLight.angle = Math.PI / 1.5;
+spotLight.decay = 0;
+scene.add(spotLight);
+scene.add(spotLight.target)
 
 const bodies = {
     sun: {interval: Interval1Hour, positions: [], isAwaiting: true, mesh: null, light: null},
@@ -53,10 +63,14 @@ async function init() {
     const sunMaterial = new THREE.MeshBasicMaterial({color: 0xffe100});
 
     bodies.sun.mesh = new THREE.Mesh(sunGeometry, sunMaterial);
-    bodies.sun.light = new THREE.DirectionalLight(0xffffff, 1);
+    bodies.sun.light = new THREE.DirectionalLight(0xffffff, 3);
+    bodies.sun.light.castShadow = true;
+    bodies.sun.light.shadow.mapSize.width = 2048;
+    bodies.sun.light.shadow.mapSize.height = 2048;
+    bodies.sun.light.shadow.bias = -0.0001;
     scene.add(bodies.sun.mesh, bodies.sun.light);
 
-    const initialSunPos = new THREE.Vector3().fromArray(bodies.sun.positions[0])
+    const initialSunPos = new THREE.Vector3().fromArray(bodies.sun.positions[0].map(num => num / 1000));
     bodies.sun.mesh.position.copy(initialSunPos);
     bodies.sun.light.position.copy(initialSunPos);
 
@@ -66,21 +80,44 @@ async function init() {
         loader.loadAsync('./assets/deimos.glb'),
     ])
 
-    bodies.mars.mesh = marsGltf.scene;
-    bodies.mars.mesh.scale.setScalar(0.002);
+    const marsGeometry = new THREE.SphereGeometry(1, 64, 32);
+    const marsSurfaceMap = textureLoader.load('./assets/images/Mars_8K_Surface.png');
+    const marsNormalMap = textureLoader.load('./assets/images/Mars_8K_Normal.png');
+    const marsMaterial = new THREE.MeshStandardMaterial({
+        map: marsSurfaceMap,
+        normalMap: marsNormalMap,
+        normalScale: new THREE.Vector2(5.0, 5.0),
+        roughness: 0.8,
+        color: 0xffc2b3,
+    });
 
+    bodies.mars.mesh = new THREE.Mesh(marsGeometry, marsMaterial);
+    bodies.mars.mesh.castShadow = true;
+    bodies.mars.mesh.receiveShadow = true;
     scene.add(bodies.mars.mesh);
 
     await getBodyPositions('phobos', true);
     bodies.phobos.mesh = phobosGltf.scene;
-    bodies.phobos.mesh.scale.setScalar(0.02);
+    bodies.phobos.mesh.scale.setScalar(0.01);
     bodies.phobos.mesh.position.fromArray(bodies.phobos.positions[0]);
+    bodies.phobos.mesh.traverse((child) => {
+        if (child.isMesh) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    })
     scene.add(bodies.phobos.mesh);
 
     await getBodyPositions('deimos', true);
     bodies.deimos.mesh = deimosGltf.scene;
-    bodies.deimos.mesh.scale.setScalar(0.02);
+    bodies.deimos.mesh.scale.setScalar(0.01);
     bodies.deimos.mesh.position.fromArray(bodies.deimos.positions[0]);
+    bodies.deimos.mesh.traverse((child) => {
+        if (child.isMesh) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    })
     scene.add(bodies.deimos.mesh);
 
     renderer.setAnimationLoop(animate);
@@ -96,7 +133,8 @@ function animate(time) {
         body.mesh.position.copy(position);
 
         if (name === "sun") {
-            body.light.position.copy(position);
+            body.light.position.copy(position.divideScalar(1000));
+            spotLight.target.position.copy(position);
         }
 
         if (stepId + 2 >= body.positions.length) {
